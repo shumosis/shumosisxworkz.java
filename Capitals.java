97536e5414b89ced762fecb36a792237452a0aec
shumosis <113335564+shumosis@users.noreply.github.com>
2022-09-14 23:56:39 +0530

    Add files via upload

---
+++ Capitals.java
@@ -0,0 +1,147 @@
+class Capitals{
+	static String getCapitalByState(String state)
+	{
+		System.out.println("entered getCapitalByState ");
+		if (state=="AndhraPradesh")
+		{
+			System.out.println("capital is hyderabad");
+			return "hyderabad";
+		}
+		if (state=="Arunachal Pradesh")
+		{
+			System.out.println("capital is itanagar");
+			return "itanagar";
+		}
+		if (state=="Assam")
+		{
+			System.out.println("capital is dispur");
+			return "dispur";
+		}
+		if (state=="Bihar")
+		{
+			System.out.println("capital is patna");
+			return "patna";
+		}
+		if (state=="Chhattisgarh")
+		{
+			System.out.println("capital is raipur");
+			return "raipur";
+		}
+		if (state=="goa")
+		{
+			System.out.println("capital is panaji");
+			return "panaji";
+		}
+		if (state=="gujarat")
+		{
+			System.out.println("capital is gandhinagar");
+			return "gandhinagar";
+		}
+		if (state=="harayana")
+		{
+			System.out.println("capital is chandigarh");
+			return "chandigarh";
+		}
+		if (state=="himachal pradesh")
+		{
+			System.out.println("capital is shimala");
+			return "shimala";
+		}
+		if (state=="jharakhand")
+		{
+			System.out.println("capital is ranchi");
+			return "ranchi";
+		}
+		if (state=="karnataka")
+		{
+			System.out.println("capital is bengaluru");
+			return "bengaluru";
+		}
+		if (state=="kerala")
+		{
+			System.out.println("capital is tiruanatpuram");
+			return "tiruanatpuram";
+		}
+		if (state=="maharashtra")
+		{
+			System.out.println("capital is mumbai");
+			return "mumbai";
+		}
+		if (state=="madhyapradesh")
+		{
+			System.out.println("capital is bhopal");
+			return "bhopal";
+		}
+		if (state=="manipur")
+		{
+			System.out.println("capital is imphal");
+			return "imphal";
+		}
+		if (state=="meghalaya")
+		{
+			System.out.println("capital is shilong");
+			return "shilong";
+		}
+		if (state=="mizoram")
+		{
+			System.out.println("capital is aizwal");
+			return "aizwal";
+		}
+		if (state=="nagaland")
+		{
+			System.out.println("capital is kohima");
+			return "kohima";
+		}
+		if (state=="odisha")
+		{
+			System.out.println("capital is bhubneshwar");
+			return "bhubneshwar";
+		}
+		if (state=="punjab")
+		{
+			System.out.println("capital is chandigarh");
+			return "chandigarh";
+		}
+		if (state=="rajsthan")
+		{
+			System.out.println("capital is udaipur");
+			return "udaipur";
+		}
+		if (state=="sikkim")
+		{
+			System.out.println("capital is gangtok");
+			return "gangtok";
+		}
+		if (state=="tamilnadu")
+		{
+			System.out.println("capital is chennai");
+			return "chennai";
+		}
+		if (state=="telangana")
+		{
+			System.out.println("capital is hyderabad");
+			return "hyderabad";
+		}
+		if (state=="tripura")
+		{
+			System.out.println("capital is agartal");
+			return "agartal";
+		}
+		if (state=="utterpradesh")
+		{
+			System.out.println("capital is luknow");
+			return "luknow";
+		}
+		if (state=="uttarakhand")
+		{
+			System.out.println("capital is deharadun");
+			return "deharadun";
+		}
+		if (state=="westbengal")
+		{
+			System.out.println("capital is kolkata");
+			return "kolkata";
+		}
+		return null;
+	}
+}
---
+++ CapitalsRunner.java
@@ -0,0 +1,84 @@
+class CapitalsRunner{
+   public static void main(String[] shumos)
+   {
+	String ref= Capitals.getCapitalByState("AndhraPradesh");
+   System.out.println("Capital of the state is"+ref) ;
+   
+   String ref1= Capitals.getCapitalByState("Arunachal Pradesh");
+   System.out.println("Capital of the state is"+ref1) ;
+   
+   String ref2= Capitals.getCapitalByState("Assam");
+   System.out.println("Capital of the state is"+ref2) ;
+   
+   String ref3= Capitals.getCapitalByState("Bihar");
+   System.out.println("Capital of the state is"+ref3) ;
+   
+   String ref4= Capitals.getCapitalByState("Chhattisgarh");
+   System.out.println("Capital of the state is"+ref4) ;
+   
+   String ref5= Capitals.getCapitalByState("goa");
+   System.out.println("Capital of the state is"+ref5) ;
+   
+   String ref6= Capitals.getCapitalByState("gujarat");
+   System.out.println("Capital of the state is"+ref6) ;
+   
+   String ref7= Capitals.getCapitalByState("harayana");
+   System.out.println("Capital of the state is"+ref7) ;
+   
+   String ref8= Capitals.getCapitalByState("himachal pradesh");
+   System.out.println("Capital of the state is"+ref8) ;
+   
+   String ref9= Capitals.getCapitalByState("jharakhand");
+   System.out.println("Capital of the state is"+ref9) ;
+   
+   String ref0= Capitals.getCapitalByState("karnataka");
+   System.out.println("Capital of the state is"+ref0) ;
+   
+   String ref11= Capitals.getCapitalByState("kerala");
+   System.out.println("Capital of the state is"+ref11) ;
+   
+   String ref12= Capitals.getCapitalByState("maharashtra");
+   System.out.println("Capital of the state is"+ref12) ;
+   
+   String ref13= Capitals.getCapitalByState("madhyapradesh");
+   System.out.println("Capital of the state is"+ref13) ;
+   
+   String ref14= Capitals.getCapitalByState("manipur");
+   System.out.println("Capital of the state is"+ref14) ;
+   
+   String ref15= Capitals.getCapitalByState("meghalaya");
+   System.out.println("Capital of the state is"+ref15) ;
+   
+   String ref16= Capitals.getCapitalByState("nagaland");
+   System.out.println("Capital of the state is"+ref16) ;
+   
+   String ref17= Capitals.getCapitalByState("odisha");
+   System.out.println("Capital of the state is"+ref17) ;
+   
+   String ref18= Capitals.getCapitalByState("punjab");
+   System.out.println("Capital of the state is"+ref18) ;
+   
+   String ref19= Capitals.getCapitalByState("rajsthan");
+   System.out.println("Capital of the state is"+ref19) ;
+   
+   String ref20= Capitals.getCapitalByState("sikkim");
+   System.out.println("Capital of the state is"+ref20) ;
+   
+   String ref21= Capitals.getCapitalByState("tamilnadu");
+   System.out.println("Capital of the state is"+ref21) ;
+   
+   String ref22= Capitals.getCapitalByState("telangana");
+   System.out.println("Capital of the state is"+ref22) ;
+   
+   String ref23= Capitals.getCapitalByState("utterpradesh");
+   System.out.println("Capital of the state is"+ref23) ;
+   
+   String ref24= Capitals.getCapitalByState("uttarakhand");
+   System.out.println("Capital of the state is"+ref24) ;
+   
+   String ref25= Capitals.getCapitalByState("westbengal");
+   System.out.println("Capital of the state is"+ref25) ;
+   
+ 
+   }
+}   
