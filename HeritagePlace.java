c183233f5631724df3d20e04c8f58a0fbcd1a8e5
shumosis <113335564+shumosis@users.noreply.github.com>
2022-09-22 23:58:13 +0530

    Add files via upload

---
+++ HeritagePlace.java
@@ -0,0 +1,18 @@
+class HeritagePLace
+{
+public static void main(String[] shumos)
+{
+	System.out.println("main nadita ede");
+	
+
+	for (int index=(0);index<shumos.length;index++)
+	{
+		String ref=shumos[index];
+	    System.out.println(ref);
+	}
+	
+
+
+
+}
+}
